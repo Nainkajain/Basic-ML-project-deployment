a63d60aaa084cd2a95846e9a640fd7481896aa42
Nainkajain <44708681+Nainkajain@users.noreply.github.com>
2022-11-08 16:24:37 +0530

    Update Simple Linear model.py

--- Simple Linear model.py
+++ Simple Linear model.py
@@ -1,101 +1,39 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "49d5e687",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[63623.43790075]\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "E:\\software\\anaconda\\lib\\site-packages\\sklearn\\base.py:450: UserWarning: X does not have valid feature names, but LinearRegression was fitted with feature names\n",
-      "  warnings.warn(\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Importing the libraries\n",
-    "import numpy as np\n",
-    "import matplotlib.pyplot as plt\n",
-    "import pandas as pd\n",
-    "import pickle\n",
-    "\n",
-    "df=pd.DataFrame({'experience':[np.nan,np.nan,'five','two','seven','three','ten','eleven'],'test_score':[8,8,6,10,9,7,np.nan,7],'interview_score':[9,6,7,10,6,10,7,8],'salary':[50000,45000,60000,65000,70000,62000,72000,80000]})\n",
-    "\n",
-    "df.to_csv('hiring.csv')\n",
-    "\n",
-    "dataset = pd.read_csv('hiring.csv')\n",
-    "\n",
-    "dataset['experience'].fillna(0, inplace=True)\n",
-    "\n",
-    "dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)\n",
-    "\n",
-    "X = dataset.iloc[:, :3]\n",
-    "\n",
-    "#Converting words to integer values\n",
-    "def convert_to_int(word):\n",
-    "    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,\n",
-    "                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}\n",
-    "    return word_dict[word]\n",
-    "\n",
-    "X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))\n",
-    "\n",
-    "y = dataset.iloc[:, -1]\n",
-    "\n",
-    "#Splitting Training and Test Set\n",
-    "#Since we have a very small dataset, we will train our model with all availabe data.\n",
-    "\n",
-    "from sklearn.linear_model import LinearRegression\n",
-    "regressor = LinearRegression()\n",
-    "\n",
-    "#Fitting model with trainig data\n",
-    "regressor.fit(X, y)\n",
-    "\n",
-    "# Saving model to disk\n",
-    "pickle.dump(regressor, open('model.pkl','wb'))\n",
-    "\n",
-    "# Loading model to compare the results\n",
-    "model = pickle.load(open('model.pkl','rb'))\n",
-    "print(model.predict([[2, 9, 6]]))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "7cfb67bc",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('hiring.csv')
+
+dataset['experience'].fillna(0, inplace=True)
+
+dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+
+X = dataset.iloc[:, :3]
+
+#Converting words to integer values
+def convert_to_int(word):
+    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
+                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
+    return word_dict[word]
+
+X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
+
+y = dataset.iloc[:, -1]
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#Fitting model with trainig data
+regressor.fit(X, y)
+
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[2, 9, 6]]))
